bc6825a02aa664e5d4c5b627f76b9ef8cd01757f
Athul Iddya <git@iddya.com>
2017-11-18 10:51:13 -0500

    Prelimnary node and tree implementation

--- com/iddya/trees/BPTNode.java
+++ com/iddya/trees/BPTNode.java
@@ -1,23 +1,196 @@
 package com.iddya.trees;
 
-public class BPTNode {
-  private NodeType type;
+import java.util.ArrayList;
 
-  public enum NodeType {
+public class BPTNode {
+private int nodeDegree;
+private Type nodeType;
+private BPTNode parent;
+private BPTNode prev;
+private BPTNode next;
+private ArrayList<KeyValue> data;
+private ArrayList<Double> keys;
+private ArrayList<BPTNode> children;
+
+public enum Type {
     Root,
     Internal,
     Leaf
-  }
+}
 
-  public BPTNode(NodeType type) {
-    this.type = type;
-  }
+public BPTNode(Type nodeType, int degree)
+{
+    this.nodeType   = nodeType;
+    this.nodeDegree = degree;
+    if (nodeType == Type.Root || nodeType == Type.Leaf) {
+        children = null;
+        data     = new ArrayList<KeyValue>();
+        keys     = null;
+    } else if (nodeType == Type.Internal) {
+        children = new ArrayList<BPTNode>();
+        data     = null;
+        keys     = new ArrayList<Double>();
+    }
+}
 
-  public NodeType getType() {
-    return this.type;
-  }
+public Type getType()
+{
+    return this.nodeType;
+}
 
-  protected void setType() {
-    this.type = type;
-  }
+protected BPTNode getParent()
+{
+    return this.parent;
+}
+
+protected void setParent(BPTNode parentNode)
+{
+    this.parent = parentNode;
+}
+
+protected BPTNode getPrev()
+{
+    if (this.nodeType == Type.Leaf) {
+        return this.prev;
+    } else {
+        return null;
+    }
+}
+
+protected void setPrev(BPTNode prevNode)
+{
+    if (this.nodeType == Type.Leaf) {
+        this.prev = prevNode;
+    }
+}
+
+protected BPTNode getNext()
+{
+    if (this.nodeType == Type.Leaf) {
+        return this.next;
+    } else {
+        return null;
+    }
+}
+
+protected void setNext(BPTNode nextNode)
+{
+    if (this.nodeType == Type.Leaf) {
+        this.next = nextNode;
+    }
+}
+
+private ArrayList<KeyValue> getData()
+{
+    return data;
+}
+
+private void setData(ArrayList<KeyValue> data)
+{
+    this.data = data;
+}
+
+protected ArrayList<Double> getKeys()
+{
+    return keys;
+}
+
+protected ArrayList<BPTNode> getChildren()
+{
+    return children;
+}
+
+protected Boolean isRoot()
+{
+    return nodeType == Type.Root;
+}
+
+protected Boolean isInternal()
+{
+    return nodeType == Type.Internal;
+}
+
+protected Boolean isLeaf()
+{
+    return nodeType == Type.Leaf;
+}
+
+public String toString()
+{
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < data.size(); i++) {
+        sb.append(data.get(i).toString());
+        sb.append(" ");
+    }
+    return sb.toString();
+}
+
+protected void insertIntoDataNode(double key, String value)
+{
+    if (!isRoot() && !isLeaf()) {
+        // Shouldn't be here
+        return;
+    }
+
+    // TODO: Clean-up the excessive cases
+    if (data.size() == 0) {
+        // Node is empty, create and add a KeyValue object
+        KeyValue newKey = new KeyValue(key, value);
+        data.add(newKey);
+    } else {
+        // Node is not empty, look for a spot to insert the new key
+        // TODO: Implement binary search
+        for (int i = 0; i < data.size(); i++) {
+            KeyValue currKV = data.get(i);
+            if (currKV.getKey() == key) {
+                // Key exists, insert value into the same key
+                currKV.addValue(value);
+                break;
+            } else if (currKV.getKey() > key) {
+                KeyValue newKey = new KeyValue(key, value);
+                // Insert before the current element
+                data.add(i, newKey);
+                break;
+            } else if (i + 1 == data.size()) {
+                // Key is the largest yet. Append at the end.
+                KeyValue newKey = new KeyValue(key, value);
+                data.add(newKey);
+                break;
+            }
+        }
+    }
+
+    // Node is overfull, need to split it
+    if (data.size() == nodeDegree) {
+        BPTNode newLeaf = new BPTNode(Type.Leaf, nodeDegree);
+
+        for (int i = nodeDegree / 2; i < nodeDegree; i++)
+            newLeaf.getData().add(data.remove(nodeDegree / 2));
+
+        if (isRoot()) {
+            // Current node is Root, create another Leaf node to add to Root
+            BPTNode newLeaf2 = new BPTNode(Type.Leaf, nodeDegree);
+            newLeaf2.setData(data);
+            data     = null;
+            children = new ArrayList<BPTNode>();
+            children.add(newLeaf2);
+            children.add(newLeaf);
+            keys = new ArrayList<Double>();
+            keys.add(newLeaf.getData().get(0).getKey());
+            newLeaf.setParent(this);
+            newLeaf2.setParent(this);
+            newLeaf2.setNext(newLeaf);
+            newLeaf.setPrev(newLeaf2);
+        } else {
+            // Current Node is Leaf, add new node to LinkedList
+            this.setNext(newLeaf);
+            newLeaf.setNext(this);
+            // Merge newLeaf.getData().get(0).getKey() to parent node
+        }
+    }
+}
+
+protected void insertIntoInternalNode(double key)
+{
+}
 }
--- com/iddya/trees/BPlusTree.java
+++ com/iddya/trees/BPlusTree.java
@@ -1,10 +1,50 @@
 package com.iddya.trees;
 
-public class BPlusTree {
-    int degree = 0;
-    BPTNode node;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
-    public BPlusTree(int degree) {
-      this.degree = degree;
+public class BPlusTree {
+int degree;
+BPTNode rootNode;
+
+public BPlusTree(int degree)
+{
+    this.degree = degree;
+    rootNode    = new BPTNode(BPTNode.Type.Root, degree);
+}
+
+public String toString()
+{
+    StringBuilder sb       = new StringBuilder();
+    Queue<BPTNode> toVisit = new LinkedList<BPTNode>();
+    toVisit.add(rootNode);
+    while (toVisit.peek() != null) {
+        BPTNode curr                = toVisit.remove();
+        ArrayList<BPTNode> children = curr.getChildren();
+        sb.append(curr.toString());
+        if (children != null) {
+            for (int i = 0; i < children.size(); i++)
+                toVisit.add(children.get(i));
+        }
     }
+    return sb.toString();
+}
+
+public void insert(double key, String value)
+{
+    System.out.println("Inserting " + key + "," + value);
+    rootNode.insertIntoDataNode(key, value);
+    System.out.println(this.toString());
+}
+
+public String search(double key)
+{
+    return "Searching for " + key;
+}
+
+public String searchRange(double startKey, double endKey)
+{
+    return "Searching from " + startKey + " to " + endKey;
+}
 }
